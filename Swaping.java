0cc87a09d7bfa160a6868e6e3b1fd8647b8a00f4
safaranees <aneesparvin08@gmail.com>
2019-05-28 11:50:52 +0530

    Create Swaping.java

---
+++ Swaping.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Swaping
+{
+	public static void main(String args[])
+	{
+		Scanner  s=new  Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int t;
+		t=a;
+		a=b;
+		b=t;
+		System.out.println(a+" "+b);
+	}
+}
